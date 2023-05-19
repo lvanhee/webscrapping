2a8ef2468a3143a15ba857a4dc1474124d8e0b2d
lvanhee <lois.vanhee@gmail.com>
2023-05-19 19:35:48 +0200

    Minor fix

--- src/webscrapping/robot/RobotUtils.java
+++ src/webscrapping/robot/RobotUtils.java
@@ -71,27 +71,25 @@ public class RobotUtils {
 	}
 
 	public static void typeString(String string) {
-		
+
 		while(!string.isEmpty())
 		{
 			char c = string.charAt(0);
-			if(c=='“'||c=='”')c='\"';
-			if(c=='’')c='\'';
 			int code = java.awt.event.KeyEvent.getExtendedKeyCodeForChar(c);
-			
+
 			boolean isShiftActive = Character.isUpperCase(c)||c=='('||c==')'|| c==':'||c=='!'||c=='\"';
 			if(isShiftActive)RobotBasedPageReader.r.keyPress(KeyEvent.VK_SHIFT);
-			
+
 			if(c=='(') code = KeyEvent.VK_9;
 			if(c=='!') code = KeyEvent.VK_1;
-			
+
 			if(c==')')code = KeyEvent.VK_0;
 			if(c=='*')code = KeyEvent.VK_MULTIPLY;
-			
+
 			if(c==':') code = KeyEvent.VK_SEMICOLON;
 			if(c=='"') code = KeyEvent.VK_QUOTE;
-			
-			if(c=='é') {
+
+			if(code==16777449) {
 				RobotBasedPageReader.r.keyPress(KeyEvent.VK_ALT);
 				RobotBasedPageReader.r.keyPress(KeyEvent.VK_NUMPAD1);
 				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD1);
@@ -101,7 +99,19 @@ public class RobotUtils {
 				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD0);
 				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_ALT);
 			}
-			else if(c=='è') {
+			else if(code==16777448) {
+				RobotBasedPageReader.r.keyPress(KeyEvent.VK_ALT);
+				RobotBasedPageReader.r.keyPress(KeyEvent.VK_NUMPAD0);
+				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD0);
+				RobotBasedPageReader.r.keyPress(KeyEvent.VK_NUMPAD2);
+				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD2);
+				RobotBasedPageReader.r.keyPress(KeyEvent.VK_NUMPAD3);
+				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD3);
+				RobotBasedPageReader.r.keyPress(KeyEvent.VK_NUMPAD2);
+				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD2);
+				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_ALT);
+			}
+			else if(c==-10000) {
 				RobotBasedPageReader.r.keyPress(KeyEvent.VK_ALT);
 				RobotBasedPageReader.r.keyPress(KeyEvent.VK_NUMPAD0);
 				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_NUMPAD0);
@@ -114,13 +124,14 @@ public class RobotUtils {
 				RobotBasedPageReader.r.keyRelease(KeyEvent.VK_ALT);
 			}
 			else {
-			RobotBasedPageReader.r.keyPress(code);
-			RobotBasedPageReader.r.keyRelease(code);
+				RobotBasedPageReader.r.keyPress(code);
+				RobotBasedPageReader.r.keyRelease(code);
 			}
-			
+
 			if(isShiftActive) RobotBasedPageReader.r.keyRelease(KeyEvent.VK_SHIFT);
 			string = string.substring(1);
-	
+
+
 		}
 	}
 
